8e68be685ced31a196f079b75d895b3b89c2254c
hengyu_laptop <1183660933@qq.com>
2025-12-20 21:48:39 +0800

    refactor&test: 补全jwtutils测试并重构AuthConstants为类

--- lab-common/src/main/java/com/hengyu/lab/common/constant/AuthConstants.java
+++ lab-common/src/main/java/com/hengyu/lab/common/constant/AuthConstants.java
@@ -1,13 +1,16 @@
 package com.hengyu.lab.common.constant;
 
+import com.hengyu.lab.common.annotations.TestIgnore;
+
+@TestIgnore
 public final class AuthConstants {
 
   private AuthConstants() {
     throw new AssertionError("No AuthConstants instances for you!");
   }
 
-   public final static String HEADER = "Authorization";
-   public final static String TOKEN_TYPE = "Bearer ";
-   public final static String TOKEN_PREFIX = "Bearer ";
+   public static final String HEADER = "Authorization";
+   public static final String TOKEN_TYPE = "Bearer";
+   public static final String TOKEN_PREFIX = "Bearer ";
 
 }

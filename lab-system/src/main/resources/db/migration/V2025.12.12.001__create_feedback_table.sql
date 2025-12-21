0fa6a8a3e14eaca8d64051c81dde556f2a78f5a8
hengyu_laptop <1183660933@qq.com>
2025-12-21 20:10:46 +0800

    style: 更改sql格式为初始状态，通过flyway检测

--- lab-system/src/main/resources/db/migration/V2025.12.12.001__create_feedback_table.sql
+++ lab-system/src/main/resources/db/migration/V2025.12.12.001__create_feedback_table.sql
@@ -1,24 +1,11 @@
-CREATE TABLE if not exists t_feedback
+CREATE TABLE  if not exists t_feedback
 (
-    id
-    BIGINT
-    NOT
-    NULL
-    AUTO_INCREMENT
-    COMMENT
-    '主键',
-    title
-    VARCHAR
-(
-    255
-) NOT NULL COMMENT '标题',
-    content TEXT COMMENT '内容',
-    status INT DEFAULT 0 COMMENT '状态',
+    id          BIGINT       NOT NULL AUTO_INCREMENT COMMENT '主键',
+    title       VARCHAR(255) NOT NULL COMMENT '标题',
+    content     TEXT COMMENT '内容',
+    status      INT      DEFAULT 0 COMMENT '状态',
     create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
     update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deleted INT DEFAULT 0,
-    PRIMARY KEY
-(
-    id
-)
-    );
+    deleted     INT      DEFAULT 0,
+    PRIMARY KEY (id)
+);

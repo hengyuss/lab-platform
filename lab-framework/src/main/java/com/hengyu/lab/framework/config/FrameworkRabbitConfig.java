7cbf0b90b8e5a160bc402ef32a0cfa31ef60f139
hengyu_laptop <1183660933@qq.com>
2026-01-29 23:29:36 +0800

    feat: 添加rabbitmq通用配置和依赖

---
+++ lab-framework/src/main/java/com/hengyu/lab/framework/config/FrameworkRabbitConfig.java
@@ -0,0 +1,64 @@
+package com.hengyu.lab.framework.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@Slf4j
+public class FrameworkRabbitConfig {
+
+
+  @Bean
+  public MessageConverter messageConverter() {
+    return new Jackson2JsonMessageConverter();
+  }
+
+
+  @Bean
+  @Primary
+  public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+    RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+    rabbitTemplate.setMessageConverter(messageConverter());
+
+    rabbitTemplate.setMandatory(true);
+
+    rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
+      if (!ack) {
+        log.error("消息未到达交换机 {}", cause);
+      }
+    });
+
+    rabbitTemplate.setReturnsCallback(returned -> {
+      log.error("消息未到达队列 RoutineKey {}, 原因 {}", returned.getRoutingKey(),
+          returned.getReplyText());
+    });
+
+    return rabbitTemplate;
+  }
+
+
+  @Bean
+  @Primary
+  public SimpleRabbitListenerContainerFactory fastFactory(ConnectionFactory connectionFactory) {
+    SimpleRabbitListenerContainerFactory simpleFactory = new SimpleRabbitListenerContainerFactory();
+    simpleFactory.setConnectionFactory(connectionFactory);
+    simpleFactory.setMessageConverter(messageConverter());
+
+    simpleFactory.setPrefetchCount(20);
+    simpleFactory.setConcurrentConsumers(5);
+    simpleFactory.setMaxConcurrentConsumers(10);
+    simpleFactory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+
+    return simpleFactory;
+  }
+
+
+}
